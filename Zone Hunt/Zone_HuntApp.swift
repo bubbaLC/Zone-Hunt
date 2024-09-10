890795c178dd65b4972382a2cba18b46bcaaa1ca
bubbaLC <liam.colton07@gmail.com>
2024-09-10 10:08:59 -0500

    Initial Commit

---
+++ Zone Hunt/Zone_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zone_HuntApp.swift
+//  Zone Hunt
+//
+//  Created by Liam Colton on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Zone_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
