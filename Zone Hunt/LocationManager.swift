a62369d2b15dfc67423bc576a6a2b80688780c9d
bubbaLC <liam.colton07@gmail.com>
2025-03-14 10:35:03 -0500

    SOOOOO many changes, tracking, location in firestore, sign-in page upon loading the app when the user isnt already signed in, profile icon in the top right of content view, and rootview

---
+++ Zone Hunt/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  Zone Hunt
+//
+//  Created by Liam Colton on 3/10/25.
+//
+
+import SwiftUI
+
+struct LocationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationManager()
+}
